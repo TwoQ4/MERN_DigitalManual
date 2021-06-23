62ea32dc498d40ed645e7563c7248e5d77aaf092
unknown <vlad.baryshev.01@mail.ru>
2021-06-24 01:00:34 +0300

    pls

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(express.json({extended: true}));
 
 app.use('/api/auth', require('./routes/auth.route'));
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, 'client', 'build')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+app.get('/*', function(req,res){
+    const fullPath = path.join(__dirname + '/dist/index.html');
+    console.log(" Fetching from.." + fullPath);
+      res.sendFile(fullPath);
+  })
 }
 /*if(process.env.NODE_ENV === 'production'){
     app.use('/', express.static(path.join(__dirname, 'client', 'build')));
